85f285003bf50ab25701b9f8a649fad3ad561c6f
emadzaheer <emadzaheer.ez@gmail.com>
2026-07-21 13:54:40 +0500

    Remove committed node_modules, build artifacts, and scratch files

--- dist/auth/auth.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { UserService } from './../user/user.service';
-export declare class AuthController {
-    private userService;
-    constructor(userService: UserService);
-    loginUser(logInDto: any): Promise<any>;
-}
